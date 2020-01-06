83f338c4b31bfce694d16e46df0f07ea97048f59
Lautaro Capella <laucape@gmail.com>
2020-01-06 20:23:31 -0300

    ignore keys

--- PKuy.js
+++ PKuy.js
@@ -3,17 +3,6 @@
   PkuyApp.ver = '1.0';
   PkuyApp.author = 'laucape@gmail.com';
   PkuyApp.tipos = {};
-  PkuyApp.gapiClientParams = {
-    "client_id": "299062314670-1g1rv86a1tcibkurn42ma93oco3ou6ul.apps.googleusercontent.com",
-    "project_id": "quickstart-1574510515702",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "redirect_uris": ["http://localhost:8000"],
-    "javascript_origins": ["http://localhost:8000"],
-    "discoveryDocs": ["https://sheets.googleapis.com/$discovery/rest?version=v4", "https://people.googleapis.com/$discovery/rest?version=v1"],
-    "scope": "https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/contacts"
-  };
   PkuyApp.dbSpreadsheet = '1O8y6zq7qmCtiWmg3Sj4VqTXlO7NQHorNWZjO3oSRlDg';
   PkuyApp.defaultSheetRows = 10000;
   PkuyApp.defaultContactsPerCall = 1000;
